8bfeeba62d824a911ee4bb1172c9fb3ce277b481
Hanna Hnida <hanna.hnida@vaimo.com>
2026-04-10 15:22:18 +0200

    feat: footer error resolved

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,7 @@
 export function Footer() {
-  const currentYear = new Date().getFullYear();
   return (
-    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
-      <aside>
-        <p>&copy;{currentYear} Vercel Daily. All rights reserved.</p>
-      </aside>
+    <footer>
+      <p>© 2026 Vercel Daily. All rights reserved.</p>
     </footer>
   );
 }
